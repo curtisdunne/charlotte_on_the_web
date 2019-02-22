38dc2a987bf59569c16b0f3e8e8b87794916aa5b
C Dunne <curtdunne@gmail.com>
2019-02-22 21:18:00 +0000

    fixed typo in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smpt_settings = {
+ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN _SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
