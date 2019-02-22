8de88447b288f01bbe282f92c55e4099c5b68793
C Dunne <curtdunne@gmail.com>
2019-02-22 21:25:18 +0000

    fixed yet another typo in environment.rb file.

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN _SMTP_SERVER'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => 'warm-depths-83125.herokuapp.com',
